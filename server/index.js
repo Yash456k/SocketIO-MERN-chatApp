aba4441993c9fd960c4ca5d9b29dd871cdf2737a
yash456k <yash456k@gmail.com>
2024-08-02 16:18:55 +0530

    add localhost for testing

--- server/index.js
+++ server/index.js
@@ -18,7 +18,10 @@ app.use(express.json());
 const server = createServer(app);
 const io = new SocketIoServer(server, {
   cors: {
-    origin: ["https://socket-io-mern-chat-app.vercel.app"],
+    origin: [
+      "https://socket-io-mern-chat-app.vercel.app",
+      "http://localhost:5173",
+    ],
 
     methods: ["GET", "POST"],
   },
@@ -30,7 +33,10 @@ const io = new SocketIoServer(server, {
 
 app.use(
   cors({
-    origin: ["https://socket-io-mern-chat-app.vercel.app"],
+    origin: [
+      "https://socket-io-mern-chat-app.vercel.app",
+      "http://localhost:5173",
+    ],
     methods: ["GET", "POST"],
   })
 );
