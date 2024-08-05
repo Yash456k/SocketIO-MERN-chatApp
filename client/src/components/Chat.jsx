e40eebfc97e00a4e328addf4a74f56cee036777b
yash456k <yash456k@gmail.com>
2024-08-05 07:33:25 +0530

    configuration

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -73,7 +73,7 @@ function Chat() {
   }, [isAi]);
 
   const initializeAiChat = async () => {
-    const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+    const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const chat = model.startChat({
       history: aiHistory,
